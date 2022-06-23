e6455253a7d4b17a87df662badca01cd80b18a59
Orhan Aytekin <contactorhanaytekin@gmail.com>
2022-06-23 10:26:04 +0300

    Added function Fib: returns the nth integer in the sequence.

--- Main.java
+++ Main.java
@@ -5,6 +5,14 @@ public class Main {
         System.out.println("*");
         Print(n-1);
     }
+
+    //returns nth fibonacci number
+    static int  Fib(int n){
+        if (n<=1) return n;
+        else return Fib(n-1)+Fib(n-2);
+    }
     public static void main(String[] args) {
+        //0 1 1 2 3 5 8 13 21
+        System.out.println(Fib(20));
     }
 }
